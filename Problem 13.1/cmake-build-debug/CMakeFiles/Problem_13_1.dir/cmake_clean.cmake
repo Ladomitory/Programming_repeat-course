2b424aad3bbc056bfcc07c35549fdbcaafc1ff76
Ladomitory <radomirholmogorov@yandex.ru>
2021-11-17 14:44:02 +0700

    Add Project "Problem 13.1"

---
+++ Problem 13.1/cmake-build-debug/CMakeFiles/Problem_13_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Problem_13_1.dir/main.cpp.obj"
+  "Problem_13_1.exe"
+  "Problem_13_1.exe.manifest"
+  "Problem_13_1.pdb"
+  "libProblem_13_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Problem_13_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
