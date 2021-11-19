2fe7b13e23361798b18a54fe7b081f4fae48e931
Ladomitory <radomirholmogorov@yandex.ru>
2021-11-19 13:32:24 +0700

    stuff in 14.3

--- Problem 14.3/main.cpp
+++ Problem 14.3/main.cpp
@@ -1,95 +1,52 @@
-///
-///Problem 14.3 (Dijkstra)
-///By Ladomitory
-///
-
 #include <iostream>
-#include <fstream>
-//#include <limits>
-#include <set>
+#include <vector>
 #include <algorithm>
+#include <iterator>
 
 using namespace std;
 
-short N, Ss, Sf;
-short ServerMap[100][100] = {0};
-
-void basicFilling() {
-    for (short i = 0; i < 100; ++i) {
-        for (short j = 0; j < 100; ++j) {
-            if (i != j) {
-                ServerMap[i][j] = INT16_MAX;
-            }
+void next(int n, vector <int> :: iterator it)
+{
+    int flag = -1;
+    for (int i = n - 2; i >= 0; --i)
+    {
+        if(*(it + i) < *(it + i + 1))
+        {
+            flag = i;
+            break;
         }
     }
+    if (flag < 0)
+        return;
+    for (int j = n - 1; j > flag; --j)
+    {
+        if (*(it + j) > *(it + flag))
+        {
+            int m = *(it + j);
+            *(it + j) = *(it + flag);
+            *(it + flag) = m;
+            break;
+        }
+    }
+    sort((it + flag + 1), (it + n - 1));
+
+    return;
 }
 
-void scanData() {
-    ifstream fin("input.txt");
-#define cin fin
-    cin >> N;
-    cin >> Ss >> Sf;
-    short x, y, k;
-    while (cin >> x >> y >> k) {
-        ServerMap[x - 1][y - 1] = k;
-        ServerMap[y - 1][x - 1] = k;
-    }
-#undef cin
-    fin.close();
-}
+int main()
+{
+    int n;
+    cin >> n;
+    vector <int> M(n);
+    for (int i = 0; i < n; i++)
+        cin >> M[i];
 
-void vectorPrint() {
-    cout << "[\n";
-    for (short i = 0; i < N; ++i) {
-        cout << "[ ";
-        for (short j = 0; j < N; ++j) {
-            cout << ServerMap[i][j] << '\t';
-        }
-        cout << "]\n";
-    }
-    cout << "]" << endl;
-}
+    vector <int> :: iterator it = M.begin();
 
-short int Dijkstra(short v0 = Ss) {
-    set<short> S;
-    S.insert(v0);
-    short D[100] = {0};
-//  D[v0] = 0;
-    for (short v = 0; v < N; ++v) {
-        if (v != v0) {
-            D[v] = ServerMap[v0][v];
-        }
-    }
+    next(n, it);
 
-    short min_val, min_i;
-    while (S.size() < N) {
-        min_val = INT16_MAX;
-        if (v0 == 1) {
-            min_i = 0;
-        } else {
-            min_i = 1;
-        }
-        for (short i = 0; i < N; ++i) {
-            if (i != v0 && S.find(i) != S.end() && D[i] < min_val) {
-                min_val = D[i];
-                min_i = i;
-            }
-        }
-        S.insert(min_i);
-        for (short v = 0; v < N; ++v) {
-            if (D[v] > D[min_i] + ServerMap[min_i][v]) {
-                D[v] = D[min_i] + ServerMap[min_i][v];
-            }
-        }
-    }
-    return D[Sf];
-}
-
-int main() {
-    basicFilling();
-    scanData();
-    vectorPrint();
-    short ans = Dijkstra();
-    cout << ans << endl;
+    for (int i = 0; i < n; i++)
+        cout << *(it + i) << " ";
+    cout << endl;
     return 0;
 }
