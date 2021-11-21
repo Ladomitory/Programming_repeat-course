6c41a46bc8eb0484ad69dc16a57f99f795dcd8b5
Ladomitory <radomirholmogorov@yandex.ru>
2021-11-21 17:35:13 +0700

    End write rough code 14.4
    Алгоритм требует оптимизации по памяти и исправления неверных ответов

--- Problem 14.4/main.cpp
+++ Problem 14.4/main.cpp
@@ -1,47 +1,60 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <string> //stoi
 #include <climits>
 
 using namespace std;
 
-void print_vec(vector <vector <int> > vec)
-{
-    cout << "\n";
-    for (int i = 0; i < vec.size(); i++)
-    {
-        for (int j = 0; j < vec.size(); j++)
-        {
-            if (vec[i][j] == INT_MAX) cout << "∞ ";
-            else cout << vec[i][j] << " ";
+void print_doubleVector(vector<vector<int>> vec) {
+    for (int i = 0; i < vec.size(); ++i) {
+        for (int j = 0; j < vec[i].size(); ++j) {
+            cout << vec[i][j] << "\t";
         }
-        cout << endl;
+        cout << '\n';
     }
-    cout << "\n";
+    cout << endl;
 }
 
 int main()
 {
-    int n;
-    cout << "Size\n";
-    cin >> n;
-    vector <vector <int> > vec(n, vector <int> (n));
-    string temp;
+    int N, M, x, y, c;
+    cin >> N >> M;
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-        {
-            cin >> temp;
-            if (temp == "inf" || temp == "-1") vec[i][j] = INT_MAX;
-            else vec[i][j] = stoi(temp);
+
+    vector<vector<vector<int>>> minWayMatric(N + 1, vector<vector<int>> (N, vector<int> (N,INT_MAX)));
+
+    for (; M > 0; M--) {
+        cin >> x >> y >> c;
+        minWayMatric[0][x - 1][y - 1] = c;
+        minWayMatric[0][y - 1][x - 1] = c;
+    }
+    for (int i = 0; i < N; ++i) {
+        minWayMatric[0][i][i] = 0;
+    }
+
+//    print_doubleVector(minWayMatric[0]);
+
+    for (int k = 1; k <= N; ++k) {
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                if (minWayMatric[k - 1][i][k - 1] == INT_MAX || minWayMatric[k - 1][k - 1][j] == INT_MAX) {
+                    minWayMatric[k][i][j] = minWayMatric[k - 1][i][j];
+                } else {
+                    minWayMatric[k][i][j] = min(minWayMatric[k - 1][i][j],
+                                                minWayMatric[k - 1][i][k - 1] + minWayMatric[k - 1][k - 1][j]);
+                }
+            }
         }
+//        print_doubleVector(minWayMatric[k]);
+    }
+//    print_doubleVector(minWayMatric[N]);
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            for (int k = 0; k < n; k++)
-                vec[i][j] = min(vec[i][j], vec[i][k] + vec[k][j]);
 
-    print_vec(vec);
+    int K;
+    cin >> K;
+    for (; K > 0; --K) {
+        cin >> x >> y;
+        cout << minWayMatric[N][x - 1][y - 1] << endl;
+    }
     return 0;
 }
