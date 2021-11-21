045fb133bb064e3bbecb48777b82530d57e05054
Ladomitory <radomirholmogorov@yandex.ru>
2021-11-21 13:29:16 +0700

    Opimize 14.3
    Переделал старый код под новую задачу, но результат 9/10(90/100)

--- Problem 14.3/main.cpp
+++ Problem 14.3/main.cpp
@@ -1,52 +1,60 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <algorithm>
-#include <iterator>
+#include <climits>
 
 using namespace std;
 
-void next(int n, vector <int> :: iterator it)
-{
-    int flag = -1;
-    for (int i = n - 2; i >= 0; --i)
-    {
-        if(*(it + i) < *(it + i + 1))
-        {
-            flag = i;
-            break;
-        }
-    }
-    if (flag < 0)
-        return;
-    for (int j = n - 1; j > flag; --j)
-    {
-        if (*(it + j) > *(it + flag))
-        {
-            int m = *(it + j);
-            *(it + j) = *(it + flag);
-            *(it + flag) = m;
-            break;
-        }
-    }
-    sort((it + flag + 1), (it + n - 1));
+vector<vector<int>> matric;
 
+void Dijkstra(int e, vector<int> *minWay)
+{
+    vector<pair<int, int>> queue; //очередь
+    vector<int> map(matric.size(), 0); //?
+    queue.push_back({e, -1});
+    int v;
+    while (!queue.empty())
+    {
+        e = queue[0].first;
+        v = queue[0].second;
+        queue.erase(queue.begin()); //взятие и удаление первого в очереди
+        if (v == -1)
+            (*minWay)[e] = 0; //если начальная вершина, то 0
+        else
+            (*minWay)[e] = min((*minWay)[v] + matric[v][e], (*minWay)[e]);
+        for (int i = 0; i < matric.size(); ++i)
+            if (i != e && matric[e][i] > 0 && (*minWay)[i] > (*minWay)[e] + matric[e][i])
+            {
+                queue.push_back({i, e});
+            }
+
+    }
     return;
 }
 
 int main()
 {
-    int n;
-    cin >> n;
-    vector <int> M(n);
-    for (int i = 0; i < n; i++)
-        cin >> M[i];
+    ifstream fin("input.txt");
 
-    vector <int> :: iterator it = M.begin();
+    int n, s, f;
+    fin >> n >> s >> f;
+    for (int i = 0; i < n; ++i) {
+        vector<int> v(n, 0);
+        matric.push_back(v);
+    }
+    int x, y, a;
+    while (fin >> x >> y >> a) {
+        matric[x - 1][y - 1] = a;
+        matric[y - 1][x - 1] = a;
+    }
 
-    next(n, it);
-
-    for (int i = 0; i < n; i++)
-        cout << *(it + i) << " ";
-    cout << endl;
+    vector<int> minWay(n, INT_MAX);
+    Dijkstra(s - 1, &minWay);
+    if (minWay[f - 1] == INT_MAX) {
+        cout << "NO" << endl;
+    } else {
+        cout << minWay[f - 1] << endl;
+    }
     return 0;
 }
