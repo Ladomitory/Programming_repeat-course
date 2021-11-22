463d8b598f9a91a36f0477975e794776ddda13dc
Ladomitory <radomirholmogorov@yandex.ru>
2021-11-22 13:48:37 +0700

    New Start Work with 10.2

--- Problem 10.2/main.cpp
+++ Problem 10.2/main.cpp
@@ -1,15 +1,11 @@
-//
-//Problem 10.2
-//By Ladomitory
-//
+////
+////Problem 10.2
+////By Ladomitory
+////
 
-// Debug Library //
 #include <iostream>
-#include <stdlib.h>
-// Read&Write Library //
 #include <fstream>
 #include <stdio.h>
-// Container Library //
 #include <string>
 #include <map>
 
@@ -18,31 +14,7 @@ using namespace std;
 const long long INF = (long long) 1.1e9;
 
 int main() {
-    // Basic Variables //
-    int arr[INF] = {0};
-    char symbol;
-
-    FILE *IF;
-    IF = fopen ("input.txt", "r");
-    // Reading Cycle //
-    int ln = 0;
-    while (fscanf(IF, "%c", &symbol) != EOF) {
-        if (symbol == ' ') {
-            arr[ln]++;
-        } else {
-            ln++;
-        }
-    }
-    fclose(IF);
-
-    ofstream OF("output.txt");
-    // Ans Output //
-    for (long long i = 0; i < INF; ++i) {
-        if (arr[i] > 0) {
-            OF << i << " - " << arr[i] << '\n';
-        }
-    }
-    OF.close();
+    
 
     return 0;
 }
