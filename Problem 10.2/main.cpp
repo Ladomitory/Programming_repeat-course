77937918fbf2bd9f87e403876d4589b9f42e685a
Ladomitory <radomirholmogorov@yandex.ru>
2021-11-24 18:31:44 +0700

    Add Ru
    Теперь он нормально определяет длину элементов из Ru в UTF-8, но что-то идёт всё ещё не так

--- Problem 10.2/main.cpp
+++ Problem 10.2/main.cpp
@@ -2,36 +2,44 @@
 ///Problem 10.2
 ///
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <clocale>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <locale>
+#include <fstream>
+#include <iostream>
 
-const unsigned int INF = (int) 1e6;
+using namespace std;
+
+const unsigned int INF = (int) 1e3;
+
+int utf8_strlen(const string& str)
+{
+    int c,i,ix,q;
+    for (q=0, i=0, ix=str.length(); i < ix; i++, q++)
+    {
+        c = (unsigned char) str[i];
+        if      (c>=0   && c<=127) i+=0;
+        else if ((c & 0xE0) == 0xC0) i+=1;
+        else if ((c & 0xF0) == 0xE0) i+=2;
+        else if ((c & 0xF8) == 0xF0) i+=3;
+            //else if (($c & 0xFC) == 0xF8) i+=4; // 111110bb //byte 5, unnecessary in 4 byte UTF-8
+            //else if (($c & 0xFE) == 0xFC) i+=5; // 1111110b //byte 6, unnecessary in 4 byte UTF-8
+        else return 0;//invalid utf8
+    }
+    return q;
+}
 
 int main() {
-    setlocale(LC_ALL, "ru_RU");
-    FILE *inp;
-    inp = fopen ("input.txt", "r");
-
-    int count = 0, k = 1;
-    unsigned char symbol;
-    int *a = (int*) calloc (INF, sizeof(int));
-    while (fscanf(inp, "%c", &symbol) != EOF) {
-        k += 1;
-        if (symbol == ' ' || symbol == '\n') {
-            a[count]++;
-            count = 0;
-        } else {
-            count++;
-        }
+    ifstream fin("input.txt");
+    string word;
+    int *a = (int*) calloc(INF, sizeof(int));
+    while (fin >> word) {
+        a[utf8_strlen(word)]++;
     }
-    a[count]++;
-    fclose (inp);
     for (int i = 1; i < INF; ++i) {
         if (a[i] != 0) {
-            printf ("%d - %d\n", i, a[i]);
+            cout << i << " - " << a[i] << endl;
         }
     }
-    free (a);
     return 0;
 }
