fce54185bc84925e5c10e3f75d6a84816c9ff8c1
Ladomitory <radomirholmogorov@yandex.ru>
2021-11-24 17:41:18 +0700

    Use C
    Попытка использовать в файлах CPP язык С, не принесла ничего нового. 3/10

--- Problem 10.2/main.cpp
+++ Problem 10.2/main.cpp
@@ -1,20 +1,37 @@
-////
-////Problem 10.2
-////By Ladomitory
-////
+///
+///Problem 10.2
+///
 
-#include <iostream>
-#include <fstream>
-#include <locale>
-#include <map>
-#include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+#include <clocale>
 
-using namespace std;
+const unsigned int INF = (int) 1e6;
 
 int main() {
-    ifstream fin("input.txt");
+    setlocale(LC_ALL, "ru_RU");
+    FILE *inp;
+    inp = fopen ("input.txt", "r");
 
+    int count = 0, k = 1;
     unsigned char symbol;
-
+    int *a = (int*) calloc (INF, sizeof(int));
+    while (fscanf(inp, "%c", &symbol) != EOF) {
+        k += 1;
+        if (symbol == ' ' || symbol == '\n') {
+            a[count]++;
+            count = 0;
+        } else {
+            count++;
+        }
+    }
+    a[count]++;
+    fclose (inp);
+    for (int i = 1; i < INF; ++i) {
+        if (a[i] != 0) {
+            printf ("%d - %d\n", i, a[i]);
+        }
+    }
+    free (a);
     return 0;
 }
