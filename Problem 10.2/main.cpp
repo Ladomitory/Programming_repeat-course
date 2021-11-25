43e3a2f7c0a76fb0d39a3f0482a44e07d389780d
Ladomitory <radomirholmogorov@yandex.ru>
2021-11-25 14:24:49 +0700

    End Solution 10.2

--- Problem 10.2/main.cpp
+++ Problem 10.2/main.cpp
@@ -2,44 +2,36 @@
 ///Problem 10.2
 ///
 
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <locale>
 #include <fstream>
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 const unsigned int INF = (int) 1e3;
-
-int utf8_strlen(const string& str)
-{
-    int c,i,ix,q;
-    for (q=0, i=0, ix=str.length(); i < ix; i++, q++)
-    {
-        c = (unsigned char) str[i];
-        if      (c>=0   && c<=127) i+=0;
-        else if ((c & 0xE0) == 0xC0) i+=1;
-        else if ((c & 0xF0) == 0xE0) i+=2;
-        else if ((c & 0xF8) == 0xF0) i+=3;
-            //else if (($c & 0xFC) == 0xF8) i+=4; // 111110bb //byte 5, unnecessary in 4 byte UTF-8
-            //else if (($c & 0xFE) == 0xFC) i+=5; // 1111110b //byte 6, unnecessary in 4 byte UTF-8
-        else return 0;//invalid utf8
-    }
-    return q;
-}
 
 int main() {
     ifstream fin("input.txt");
     string word;
     int *a = (int*) calloc(INF, sizeof(int));
     while (fin >> word) {
-        a[utf8_strlen(word)]++;
+        a[strlen(word.c_str())]++;
+        //Судя по всему сохраняемое при заполнении string.length() считается криво
+        //А strlen перебирает их заново. Но работает, только с char*, по этому используем преобразование
+        //Почему это происходит? Вопрос хороший, ответ я не нашёл.
+        //Илья Андреевич, если вы это смотрите, объясните почему так? И как можно было иначе?
     }
+    fin.close();
+
+    ofstream fout;
+    fout.open("output.txt");
     for (int i = 1; i < INF; ++i) {
         if (a[i] != 0) {
-            cout << i << " - " << a[i] << endl;
+            fout << i << " - " << a[i] << endl;
         }
     }
+    fout.close();
+
+    free(a);
     return 0;
 }
